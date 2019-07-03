8e3cf564c60a1ea055846c7ce1cece07a2056e56
Amira Bouguera <amira.bouguera@consensys.net>
2019-07-03 23:01:07 +0200

    update

--- src/gadgets/preimage_proof_gadget.hpp
+++ src/gadgets/preimage_proof_gadget.hpp
@@ -4,12 +4,11 @@
 */
 
 
-#include <libsnark/gadgetlib1/gadget.hpp>
-#include <libsnark/gadgetlib1/gadgets/basic_gadgets.hpp>
+#include <libsnark/gadgetlib1/gadgets/basic_gadgets.hpp> // for multipacking gadget
 #include <libsnark/gadgetlib1/gadgets/hashes/sha256/sha256_gadget.hpp>
-#include <libff/algebra/fields/field_utils.hpp>
-#include <libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp>
-#include <libsnark/gadgetlib1/protoboard.hpp>
+#include <libff/algebra/fields/field_utils.hpp> 
+#include <libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp> 
+
 
 
 
@@ -47,26 +46,20 @@ template<typename FieldT>
 
 
     public:
-    //static const size_t SHA256_digest_size_bytes = libsnark::SHA256_digest_size / 8;
 
-
-
-        // Verifier inputs
+    // Verifier inputs
 
     const pb_variable_array<FieldT> input_as_field_elements; /* R1CS input */
     const pb_variable_array<FieldT> input_as_bits;  // unpacked R1CS input since these values
     const size_t input_size_in_fields;
     shared_ptr<libsnark::multipacking_gadget<FieldT>> unpacker;
-    //The sha256_compression_function_gadget only implements the SHA256 compression function. 
-    typedef  shared_ptr<sha256_compression_function_gadget<FieldT>> sha256_gadget;
-    shared_ptr<digest_variable<FieldT>> h_result;   
-    shared_ptr<block_variable<FieldT>>  block;
-    shared_ptr<digest_variable<FieldT>> preimage; 
+    shared_ptr<sha256_compression_function_gadget<FieldT>> sha256_gadget; /* hashing gadget */
+    shared_ptr<digest_variable<FieldT>> Hash;  
+    shared_ptr<block_variable<FieldT>>  block; /* 512 bit block that contains preimage + padding */
+    shared_ptr<digest_variable<FieldT>> preimage;  
     shared_ptr<digest_variable<FieldT>> computed_hash; 
-    shared_ptr<sha256_compression_function_gadget<FieldT>> hash; // the sha256 gadget
-    pb_variable<FieldT> zero; 
-    pb_variable_array<FieldT> padding_var;
-
+    pb_variable_array<FieldT> padding_var; 
+    pb_variable<FieldT> zero;  
 
 
 
@@ -75,7 +68,7 @@ template<typename FieldT>
 
     preimage_proof_gadget( protoboard<FieldT>& pb,
 
-        const digest_variable<FieldT> & h_result,
+        const digest_variable<FieldT> & Hash,
         const digest_variable<FieldT> & preimage,     
         const string &annotation_prefix                    
 
@@ -86,11 +79,13 @@ template<typename FieldT>
 
         ) : gadget<FieldT>(pb, "preimage_proof_gadget")
 
-    {         const size_t input_size_in_bits = sha256_digest_len; 
+             // Allocate space for the verifier input
+    {         const size_t input_size_in_bits = sha256_digest_len; // probably should check if digest size is 256
 
 
         {
-                // number of field packed elements as input
+            
+            // number of field packed elements as public inputs
 
             input_size_in_fields = libff::div_ceil(input_size_in_bits, FieldT::capacity()); 
 
@@ -103,7 +98,7 @@ template<typename FieldT>
                   */
 
 
-                // we allocate space for the field elements to be of size input_size_in_field_elements
+            // we allocate space for the field elements to be of size input_size_in_field_elements
 
             input_as_field_elements.allocate(pb, input_size_in_fields, "public_inputs");
 
@@ -116,21 +111,24 @@ template<typename FieldT>
 
         zero.allocate(this->pb, FMT(this->annotation_prefix, "zero"));
 
+        // Verifier (and prover) inputs:
+        Hash.reset(new digest_variable<FieldT>(pb, sha256_digest_len, "Hash"));
+        input_as_bits.insert(input_as_bits.end(), Hash.bits.begin(), Hash.bits.end());
 
-        input_as_bits.insert(input_as_bits.end(), h_result.bits.begin(), h_result.bits.end());
-
-
-
+        assert(input_as_bits.size() == input_size_in_bits);
+        // multipacking_gadget(protoboard<FieldT> &pb, const pb_linear_combination_array<FieldT> &bits, const pb_linear_combination_array<FieldT> &packed_vars, const size_t chunk_size, const std::string &annotation_prefix="");
         unpacker.reset(new multipacking_gadget<FieldT>(pb, input_as_bits, input_as_field_elements, FieldT::capacity(), FMT(this->annotation_prefix, " unpacker")));
+        
+        // Prover inputs:
+        preimage.reset(new digest_variable<FieldT>(pb, sha256_digest_len, "preimage"));
 
 
-
+        // IV for SHA256
         pb_linear_combination_array<FieldT> IV = SHA256_default_IV(pb);
 
-
-
         
-            // SHA256's length padding
+        
+        // SHA256's length padding
         for (size_t i = 0; i < 256; i++) {
             if (sha256_padding[i])
                 padding_var.emplace_back(ONE);
@@ -140,28 +138,27 @@ template<typename FieldT>
         
 
 
-        // Inputs are 256 bit padding and 256 bit message block
+        // Initialize the block gadget for preimage's hash
         block.reset(new block_variable<FieldT>(pb, {
            preimage->bits,
            padding_var
        }, "block"));
-
+        // check what assert function does
         assert(block.bits.size() == SHA256_block_size);
 
 
-        // Inputs are 256 bit IV and 512 bit h_block (64 bytes)
+        
 
         computed_hash.reset(new digest_variable<FieldT>(pb, sha256_digest_len, FMT(this->annotation_prefix, " computed_hash")));
-        hash.reset(new sha256_compression_function_gadget<FieldT>(pb,
+        
+        // Initialize the hash gadget for preimage's hash
+        sha256_gadget.reset(new sha256_compression_function_gadget<FieldT>(pb,
             IV,
             block->bits,
             *computed_hash,
             FMT(this->annotation_prefix, "computed_hash")));
 
 
-        assert(computed_hash.size() == sha256_digest_len);
-
-        assert(input_as_bits.size() == input_size_in_bits);
 
 
     }
@@ -169,17 +166,19 @@ template<typename FieldT>
 
     void generate_r1cs_constraints()
     {
-
+        // Multipacking constraints (for input validation) with enforcing bitness
         unpacker.generate_r1cs_constraints(true);
-        // Sanity check
+        preimage->generate_r1cs_constraints();
+        // Sanity check, what is this sanity check and why do we need it?
         generate_r1cs_equals_const_constraint<FieldT>(this->pb, zero, FieldT::zero(), "zero");
+        
+        // Constraints to ensure the hash validates.
+        sha256_gadget->generate_r1cs_constraints(false); 
 
-        hash->generate_r1cs_constraints(false); /* ensure correct hash computations */
 
-        // Constraint that computed_root * 1 == rootDigest which is equivalent to computed_root == rootDigest
-
-        this->pb.add_r1cs_constraint(r1cs_constraint<FieldT>(1, computed_hash, h_result), "Enforce valid proof");
-
+        // ensure correct hash computations 
+        this->pb.add_r1cs_constraint(r1cs_constraint<FieldT>(1, computed_hash, Hash), "Enforce valid proof");
+     // Constraint that computed_root * 1 == rootDigest which is equivalent to computed_root == rootDigest
     }
 
 
@@ -195,13 +194,13 @@ template<typename FieldT>
         this->pb.val(zero) = FieldT::zero();
 
         // Generate witnesses as necessary in our gadgets
+        sha256_gadget->generate_r1cs_witness();
+        unpacker->generate_r1cs_witness_from_bits(); 
+        //unpacker->generate_r1cs_witness_from_packed();
+        
 
 
-        hash->generate_r1cs_witness();
-        unpacker->generate_r1cs_witness_from_bits();
-
-
-        h_result->bits.fill_with_bits(this->pb, pub_hash);
+        Hash->bits.fill_with_bits(this->pb, pub_hash); 
 
 
 
@@ -213,29 +212,28 @@ template<typename FieldT>
 
 template<typename FieldT>
 
-    // The statement (public values) is called primary input while the witness (the secret values) is called auxiliary input.
+// The statement (public values) is called primary input while the witness (the secret values) is called auxiliary input.
 
 const r1cs_primary_input<FieldT> primary_inputs (const bit_vector &pub_hash)
 
 
 
 {
+   // Construct the multipacked field points which encode
+    // the verifier's knowledge. This is the "dual" of the
+    // multipacking gadget logic in the constructor.
+    assert(pub_hash.size() == sha256_digest_len);
 
-
-
-
-  //  assert(pub_hash.bits.size() == sha256_digest_len);
+    std::cout << "**** After assert(size() == sha256_digest_len) *****" << std::endl;
 
     bit_vector input_as_bits;
-
     input_as_bits.insert(input_as_bits.end(), pub_hash.begin(), pub_hash.end());
 
 
 
-    std::cout << "**** After assert(size() == sha256_digest_len) *****" << std::endl;
-
-
+    /* std::vector<FieldT> pack_bit_vector_into_field_element_vector(const bit_vector &v, const size_t chunk_bits) */
     std::vector<FieldT> input_as_field_elements = pack_bit_vector_into_field_element_vector<FieldT>(input_as_bits);
+    
 
 
     std::cout << "**** After pack_bit_vector_into_field_element_vector *****" << std::endl;
@@ -245,9 +243,4 @@ const r1cs_primary_input<FieldT> primary_inputs (const bit_vector &pub_hash)
 
 
 }
-
-
-
-
-
 
